59792c809bb4e725231ec57739899cc38bb8e9de
weeger <romain.weeger@gmail.com>
2025-12-29 14:22:00 +0100

    Move demo and test

---
+++ src/Controller/Pages/DemoController.php
@@ -0,0 +1,123 @@
+<?php
+
+namespace Wexample\SymfonyLoaderTesting\Controller\Pages;
+
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Wexample\SymfonyHelpers\Attribute\SimpleRoutesController;
+use Wexample\SymfonyLoader\Controller\AbstractPagesController;
+use Wexample\SymfonyLoader\Rendering\RenderPass;
+use Wexample\SymfonyLoader\Service\Usage\FontsAssetUsageService;
+use Wexample\SymfonyLoader\Traits\SymfonyLoaderBundleClassTrait;
+use Wexample\SymfonyHelpers\Helper\VariableHelper;
+
+#[Route(path: '_loader/demo/', name: '_loader_demo_')]
+#[SimpleRoutesController]
+final class DemoController extends AbstractPagesController
+{
+    use SymfonyLoaderBundleClassTrait;
+
+    final public const ROUTE_INDEX = VariableHelper::INDEX;
+    final public const ROUTE_ASSETS = VariableHelper::ASSETS;
+    final public const ROUTE_AGGREGATION = 'aggregation';
+    final public const ROUTE_COLOR_SCHEMES = 'color_schemes';
+    final public const ROUTE_ICONS = 'icons';
+    final public const ROUTE_LOADING = VariableHelper::LOADING;
+    final public const ROUTE_LOADING_FETCH_SIMPLE = VariableHelper::LOADING.'_fetch_simple';
+    final public const ROUTE_TRANSLATIONS = VariableHelper::TRANSLATIONS;
+    final public const ROUTE_COMPONENTS = VariableHelper::PLURAL_COMPONENT;
+
+    private bool $useJs = true;
+
+    public static function getSimpleRoutes(): array
+    {
+        return [
+            self::ROUTE_COMPONENTS,
+            self::ROUTE_ICONS,
+            self::ROUTE_LOADING,
+            self::ROUTE_LOADING_FETCH_SIMPLE,
+            self::ROUTE_TRANSLATIONS,
+        ];
+    }
+
+    #[Route(path: '', name: self::ROUTE_INDEX)]
+    final public function index(): Response
+    {
+        return $this->renderPage(
+            self::ROUTE_INDEX
+        );
+    }
+
+    protected function configureRenderPass(
+        RenderPass $renderPass
+    ): RenderPass {
+        $renderPass->setUseJs($this->useJs);
+
+        $renderPass->setUsage(
+            FontsAssetUsageService::getName(),
+            'demo'
+        );
+
+        return $renderPass;
+    }
+
+    #[Route(
+        path: VariableHelper::ASSETS,
+        name: self::ROUTE_ASSETS
+    )]
+    final public function assets(Request $request): Response
+    {
+        $this->useJs = !$request->get('no_js');
+
+        return $this->renderPage(
+            self::ROUTE_ASSETS,
+        );
+    }
+
+    #[Route(
+        path: self::ROUTE_AGGREGATION,
+        name: self::ROUTE_AGGREGATION
+    )]
+    final public function aggregation(): Response
+    {
+        // Prepare specific render pass.
+        $renderPass = $this->createRenderPass(
+            $this->buildControllerTemplatePath(
+                self::ROUTE_AGGREGATION,
+            )
+        );
+        $renderPass->enableAggregation = true;
+
+        return $this->renderPage(
+            self::ROUTE_AGGREGATION,
+            renderPass: $renderPass
+        );
+    }
+
+    #[Route(
+        path: 'color-schemes',
+        name: self::ROUTE_COLOR_SCHEMES
+    )]
+    final public function colorSchemes(): Response
+    {
+        // Prepare specific render pass.
+        $renderPass = $this->createRenderPass(
+            $this->buildControllerTemplatePath(
+                self::ROUTE_COLOR_SCHEMES,
+            )
+        );
+
+        // Allow every usage switch.
+        foreach ($renderPass->usagesConfig as &$config) {
+            foreach ($config['list'] as &$item) {
+                $item['allow_switch'] = true;
+            }
+        }
+
+        return $this->renderPage(
+            self::ROUTE_COLOR_SCHEMES,
+            renderPass: $renderPass
+        );
+    }
+}
---
+++ src/Controller/Pages/TestController.php
@@ -0,0 +1,79 @@
+<?php
+
+namespace Wexample\SymfonyLoaderTesting\Controller\Pages;
+
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Wexample\SymfonyLoader\Controller\AbstractPagesController;
+use Wexample\SymfonyLoader\Service\Usage\FontsAssetUsageService;
+use Wexample\SymfonyLoader\Traits\SymfonyLoaderBundleClassTrait;
+use Wexample\SymfonyHelpers\Helper\VariableHelper;
+
+#[Route(path: '_loader/test/', name: '_loader_test_')]
+final class TestController extends AbstractPagesController
+{
+    use SymfonyLoaderBundleClassTrait;
+
+    final public const ROUTE_ADAPTIVE = 'adaptive';
+    final public const ROUTE_ERROR_MISSING_VIEW = 'error_missing_view';
+    final public const ROUTE_INDEX = VariableHelper::INDEX;
+    final public const ROUTE_VIEW = VariableHelper::VIEW;
+
+    #[Route(path: '', name: self::ROUTE_INDEX)]
+    final public function index(Request $request): Response
+    {
+        $renderPass = $this->createRenderPass(
+            $this->buildControllerTemplatePath(
+                self::ROUTE_INDEX,
+            )
+        );
+
+        $renderPass->setUsage(
+            FontsAssetUsageService::getName(),
+            'demo'
+        );
+
+        $renderPass->enableAggregation = $request->get('test-aggregation', false);
+
+        return $this->renderPage(
+            self::ROUTE_INDEX,
+            renderPass: $renderPass
+        );
+    }
+
+    #[Route(path: self::ROUTE_ADAPTIVE, name: self::ROUTE_ADAPTIVE, options: self::ROUTE_OPTIONS_ONLY_EXPOSE)]
+    final public function adaptive(Request $request): Response
+    {
+        $renderPass = $this->createRenderPass(
+            $this->buildControllerTemplatePath(
+                self::ROUTE_ADAPTIVE,
+            )
+        );
+
+        if ($request->get('no-js')) {
+            $renderPass->setUseJs(false);
+        }
+
+        return $this->renderPage(
+            self::ROUTE_ADAPTIVE,
+            renderPass: $renderPass
+        );
+    }
+
+    #[Route(path: self::ROUTE_VIEW, name: self::ROUTE_VIEW, options: self::ROUTE_OPTIONS_ONLY_EXPOSE)]
+    public function view(): Response
+    {
+        return $this->renderPage(
+            self::ROUTE_VIEW,
+            bundle: self::getBundleClassName());
+    }
+
+    #[Route(path: 'error-missing-view', name: self::ROUTE_ERROR_MISSING_VIEW, options: self::ROUTE_OPTIONS_ONLY_EXPOSE)]
+    public function errorMissingVue(): Response
+    {
+        return $this->renderPage(
+            self::ROUTE_ERROR_MISSING_VIEW
+        );
+    }
+}
