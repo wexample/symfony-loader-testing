9877745e440470c44b633c3fd480e43b2c18e428
weeger <romain.weeger@gmail.com>
2026-02-05 14:24:42 +0100

    Modal service

--- assets/pages/demo/loading.ts
+++ assets/pages/demo/loading.ts
@@ -1,5 +1,5 @@
 import Page from '@wexample/symfony-loader/js/Class/Page';
-import ModalsService from '@wexample/symfony-loader/js/Services/ModalsService';
+import ModalService from '@wexample/symfony-loader/js/Services/ModalService';
 import AppService from '@wexample/symfony-loader/js/Class/AppService';
 import ServicesRegistryInterface from '@wexample/symfony-loader/js/Interfaces/ServicesRegistryInterface';
 
@@ -7,12 +7,12 @@ export default class extends Page {
   services: ServicesRegistryInterface;
 
   getPageLevelServices(): typeof AppService[] {
-    return [ModalsService];
+    return [ModalService];
   }
 
   async pageReady() {
     this.el.querySelector('#page-modal-show').addEventListener('click', () => {
-      (this.app.getService(ModalsService) as ModalsService).get('/_loader/demo/loading-fetch-simple');
+      (this.app.getService(ModalService) as ModalService).get('/_loader/demo/loading-fetch-simple');
     });
   }
 }
--- assets/pages/demo/loading_fetch_simple.ts
+++ assets/pages/demo/loading_fetch_simple.ts
@@ -1,5 +1,5 @@
 import Page from '@wexample/symfony-loader/js/Class/Page';
-import ModalsService from '@wexample/symfony-loader/js/Services/ModalsService';
+import ModalService from '@wexample/symfony-loader/js/Services/ModalService';
 import AppService from '@wexample/symfony-loader/js/Class/AppService';
 import ServicesRegistryInterface from '@wexample/symfony-loader/js/Interfaces/ServicesRegistryInterface';
 
@@ -7,14 +7,14 @@ export default class extends Page {
   services: ServicesRegistryInterface;
 
   getPageLevelMixins(): typeof AppService[] {
-    return [ModalsService];
+    return [ModalService];
   }
 
   async pageReady() {
     this.el
       .querySelector('.open-another-modal')
       .addEventListener('click', () => {
-        (this.app.getService(ModalsService) as ModalsService)
+        (this.app.getService(ModalService) as ModalService)
           .get('/_loader/demo/loading-fetch-simple');
       });
   }
