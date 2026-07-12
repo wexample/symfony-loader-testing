ab2d5a47a190b0456abe5c2fa3ebc21eb53e9959
weeger <romain.weeger@gmail.com>
2026-07-12 14:01:22 +0200

    Menu

--- src/Controller/Pages/DemoController.php
+++ src/Controller/Pages/DemoController.php
@@ -4,7 +4,7 @@ namespace Wexample\SymfonyLoaderTesting\Controller\Pages;
 
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Routing\Attribute\Route;
 use Wexample\SymfonyHelpers\Attribute\SimpleRoutesController;
 use Wexample\SymfonyHelpers\Helper\VariableHelper;
 use Wexample\SymfonyLoader\Controller\AbstractPagesController;
