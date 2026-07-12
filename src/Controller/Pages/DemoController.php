e9e6bdd2029b07c3bee705131066f0f4408a6b8c
weeger <romain.weeger@gmail.com>
2026-07-12 15:12:42 +0200

    Cleanup

--- src/Controller/Pages/DemoController.php
+++ src/Controller/Pages/DemoController.php
@@ -25,7 +25,6 @@ final class DemoController extends AbstractPagesController
     final public const ROUTE_ICONS = 'icons';
     final public const ROUTE_LOADING = VariableHelper::LOADING;
     final public const ROUTE_LOADING_FETCH_SIMPLE = VariableHelper::LOADING . '_fetch_simple';
-    final public const ROUTE_TRANSLATIONS = VariableHelper::TRANSLATIONS;
     final public const ROUTE_COMPONENTS = VariableHelper::PLURAL_COMPONENT;
 
     private bool $useJs = true;
@@ -37,7 +36,6 @@ final class DemoController extends AbstractPagesController
             self::ROUTE_ICONS,
             self::ROUTE_LOADING,
             self::ROUTE_LOADING_FETCH_SIMPLE,
-            self::ROUTE_TRANSLATIONS,
         ];
     }
 
---
+++ src/Controller/Pages/TranslationsController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Wexample\SymfonyLoaderTesting\Controller\Pages;
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+use Wexample\SymfonyHelpers\Helper\VariableHelper;
+use Wexample\SymfonyLoader\Controller\AbstractPagesController;
+use Wexample\SymfonyLoaderTesting\Traits\SymfonyLoaderTestingBundleClassTrait;
+
+#[Route(path: '_loader/translations/', name: '_loader_translations_')]
+final class TranslationsController extends AbstractPagesController
+{
+    use SymfonyLoaderTestingBundleClassTrait;
+
+    final public const ROUTE_INDEX = VariableHelper::INDEX;
+
+    #[Route(path: '', name: self::ROUTE_INDEX)]
+    final public function index(): Response
+    {
+        return $this->renderPage(self::ROUTE_INDEX);
+    }
+}
