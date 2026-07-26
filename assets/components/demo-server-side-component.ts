0f4a84afe998ddaddc1f04d59d55140aba40165a
weeger <romain.weeger@gmail.com>
2026-07-26 14:55:10 +0200

    Cleanup

--- assets/components/demo-server-side-component.ts
+++ assets/components/demo-server-side-component.ts
@@ -4,6 +4,9 @@ export default class extends Component {
   async mounted() {
     await super.mounted();
 
-    console.log('Server side rendered component mounted.')
+    const status = document.createElement('span');
+    status.className = 'status status--success';
+    status.textContent = '✓ Component loaded';
+    this.el.prepend(status);
   }
 }
