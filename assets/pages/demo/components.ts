8fbfa327251575eb977478e671714f03941b61af
weeger <romain.weeger@gmail.com>
2026-07-27 16:38:02 +0200

    Cleanup

--- assets/pages/demo/components.ts
+++
@@ -1,8 +0,0 @@
-import Page from '@wexample/symfony-loader/js/Class/Page';
-import { initCodeBlocks } from '@wexample/symfony-content/ts/code-block';
-
-export default class extends Page {
-  async pageReady() {
-    await initCodeBlocks(this.el);
-  }
-}
--- src/Controller/Pages/DemoController.php
+++ src/Controller/Pages/DemoController.php
@@ -4,7 +4,6 @@ namespace Wexample\SymfonyLoaderTesting\Controller\Pages;
 
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
-use Wexample\SymfonyHelpers\Attribute\SimpleRoutesController;
 use Wexample\SymfonyHelpers\Helper\VariableHelper;
 use Wexample\SymfonyLoader\Controller\AbstractPagesController;
 use Wexample\SymfonyLoader\Rendering\RenderPass;
@@ -12,22 +11,13 @@ use Wexample\SymfonyLoader\Service\Usage\FontsAssetUsageService;
 use Wexample\SymfonyLoaderTesting\Traits\SymfonyLoaderTestingBundleClassTrait;
 
 #[Route(path: '_loader/demo/', name: '_loader_demo_')]
-#[SimpleRoutesController]
 final class DemoController extends AbstractPagesController
 {
     use SymfonyLoaderTestingBundleClassTrait;
 
     final public const ROUTE_INDEX = VariableHelper::INDEX;
-    final public const ROUTE_COMPONENTS = VariableHelper::PLURAL_COMPONENT;
 
     private bool $useJs = true;
-
-    public static function getSimpleRoutes(): array
-    {
-        return [
-            self::ROUTE_COMPONENTS,
-        ];
-    }
 
     #[Route(path: '', name: self::ROUTE_INDEX)]
     final public function index(): Response
@@ -49,5 +39,4 @@ final class DemoController extends AbstractPagesController
 
         return $renderPass;
     }
-
 }
